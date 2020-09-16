74d936de03b4856e1b77f085cc6f33ac70f59122
brandon barreto <diegodjsonic@gmail.com>
2020-09-16 17:45:56 -0500

    terminado

--- caballo.py
+++ caballo.py
@@ -30,10 +30,46 @@ def coordLegal(x,tamanoTablero):
     else:
         return False
 
+
+def giraCaballo(n,ruta,u,limite):
+    u.asignarColor('gris')
+    ruta.append(u)
+    if n < (limite -1) :
+        listaVecinos = list(u.obtenerConexiones())
+        i = 0
+        hecho = False
+        while i < len(listaVecinos) and not hecho:
+            if listaVecinos[i].obtenerColor() == 'blanco':
+                hecho = giraCaballo(n+1, ruta, listaVecinos[i], limite)
+            i = i + 1
+        if not hecho:  # prepararse para retroceder
+            ruta.pop()
+            u.asignarColor('blanco')
+    else:
+        hecho = True
+    return hecho
+
 g = grafoDelCaballo(8)
 
+
+
+
+"""
 print("-------------------------------")
 print( g.listaVertices )
 for v in g:
     for w in v.obtenerConexiones():
         print("( %s , %s )" % (v.obtenerId(), w.obtenerId()))
+
+print("")
+print("-------------------------------")
+"""
+lista = g.listaVertices
+n = 0
+ruta = []
+u = lista[4]
+limite = 64
+
+giraCaballo(n,ruta,u,limite)
+
+print(  [x.id for x in ruta] )
